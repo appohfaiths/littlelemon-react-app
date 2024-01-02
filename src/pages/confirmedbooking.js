d88263d2784b45b5cb697cf3f6259a98efd68d17
KodjoCode <appohfaiths@gmail.com>
2024-01-02 20:08:30 +0000

    feat: add back to home button

--- src/pages/confirmedbooking.js
+++ src/pages/confirmedbooking.js
@@ -1,12 +1,18 @@
 import React from 'react'
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function ConfirmedBooking() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  }
   return (
       <main className='confirmed-booking'>
           <h1>Booking Confirmed</h1>
           <p>Please provide your name and booking number at the reception upon arrival</p>
-          <p>Little Lemon!</p>
+      <p>Little Lemon!</p>
+      <button onClick={handleClick} className='back-to-home'>Back to Home</button>
     </main>
   )
 }
